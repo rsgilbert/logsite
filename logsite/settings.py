33a313bae44812c00849ceb706d665ca6609942d
gilbert <gssenyonjo@gmail.com>
2019-08-19 13:48:11 +0300

    registered User model, installed the account and logs apps

--- logsite/settings.py
+++ logsite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
+    'logs.apps.LogsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
