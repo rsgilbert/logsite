1868a98c55968542368ab1df7828f7409e34beb6
gilbert <gssenyonjo@gmail.com>
2019-08-20 11:41:36 +0300

    created model Log

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from account.models import User
+
 
 # Create your models here.
+
+
+class Log(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    log = models.CharField(max_length=1000)
